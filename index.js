01cdc77616446dcad8a49ad080c751520c0a7078
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-04-23 14:07:09 +0530

    test commit

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ import {AppRegistry, LogBox} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 LogBox.ignoreAllLogs();
+// console.log(test)
 AppRegistry.registerComponent(appName, () => App);
